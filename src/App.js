347174135f03687f9e425999f5c0dd2cc588dbb6
kartmatias <kartmatias@gmail.com>
2020-07-27 17:09:45 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      Ola mundo\const name = new type(arguments);
+      Ok
+      Tudo junto
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../../assets/img/kartflix.png'
+
+function Menu() {
+    return (
+        <header>
+            KartFlix
+        </header>
+
+    );
+}
+
+
+export default Menu;
